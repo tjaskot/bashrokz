9a1d0db1e4c7479e74afa5d48e87e3516ba63918
tjaskot <trevord186@gmail.com>
2020-03-20 17:21:36 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -15,3 +15,25 @@ do
 		#sed -e 's/\[^]]*\]//'
 	fi
 done
+
+#echo 'current dir: '${PWD##*/}
+echo 'checking cur dir'
+misdir="/mnt/c/Users/trevo/Documents/mis/test"
+if [[ $misdir != $PWD ]]; then
+        echo 'go to mis/test dir'
+        exit 1
+fi
+echo 'copying * from ../'
+for upf in ../*; do
+        #echo $upf
+        if [[ $upf != '../test' ]]; then
+                cp $upf .
+        fi
+done
+echo 'mv names'
+for i in *; do
+        #echo $i
+        if [[ $i == *.ts ]]; then
+                mv $i $i't'
+        fi
+done
