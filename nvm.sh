36993c9673278e8a29b9f37cd969637593919c34
tjaskot <trevord186@gmail.com>
2024-09-03 10:58:14 -0500

    Update nvm.sh

--- nvm.sh
+++ nvm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# with HISTCONTROL=ignoreboth on, if you add space in front of 'nvm' command like ' nvm', it will remove 2 items in history since terminal execution doesn't get added to history
 seqEnd=$1
 if [[ $seqEnd == "" ]]; then
 	# Need a second item because history incremented with "nvm" from terminal execution, and need to delete this plus one line back
